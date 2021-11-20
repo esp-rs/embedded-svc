5ebb322a3b8f09635acd25c1fe19b35791fe9648
untrade <untradeta@gmail.com>
2021-11-20 20:35:29 +0200

    added hostname field to ClientConfiguration

--- src/wifi.rs
+++ src/wifi.rs
@@ -211,6 +211,7 @@ pub struct ClientConfiguration {
     pub password: alloc::string::String,
     pub channel: Option<u8>,
     pub ip_conf: Option<ipv4::ClientConfiguration>,
+	pub hostname: Option<alloc::string::String>
 }
 
 impl ClientConfiguration {
@@ -243,6 +244,7 @@ impl Default for ClientConfiguration {
             password: "".into(),
             channel: None,
             ip_conf: Some(Default::default()),
+			hostname: None
         }
     }
 }
