48ee553e47e20f341f032359083b774f641c4baf
ivmarkov <ivan.markov@gmail.com>
2022-04-16 14:14:27 +0300

    Abstraction for executors with lockless task queues

--- src/utils/asyncs/executor.rs
+++ src/utils/asyncs/executor.rs
@@ -1,5 +1,7 @@
+use core::borrow::Borrow;
 use core::future::Future;
 use core::marker::PhantomData;
+use core::ptr;
 
 extern crate alloc;
 use alloc::rc::Rc;
@@ -11,14 +13,14 @@ use crossbeam_queue::ArrayQueue;
 
 use super::signal::*;
 
-use crate::mutex::SingleThreadedMutex;
+use crate::mutex::{MutexFamily, SingleThreadedMutex};
 use crate::signal::asyncs::Signal;
 
-pub trait Waiter {
+pub trait Wait {
     fn wait(&self);
 }
 
-impl<F> Waiter for F
+impl<F> Wait for F
 where
     F: Fn(),
 {
@@ -27,11 +29,15 @@ where
     }
 }
 
-pub trait Notifier {
+pub trait Notify {
+    fn prerun(&self) {}
+
     fn notify(&self);
+
+    fn postrun(&self) {}
 }
 
-impl<F> Notifier for F
+impl<F> Notify for F
 where
     F: Fn(),
 {
@@ -40,34 +46,114 @@ where
     }
 }
 
-pub struct LocalExecutor<'a, W, N> {
-    queue: Arc<ArrayQueue<Runnable>>,
-    waiter: W,
-    notifier: N,
-    _lft: PhantomData<&'a ()>,
-}
+pub struct LocalExecutor<'a, W, N>(Executor<'a, W, N>, *const ());
 
 impl<'a, W, N> LocalExecutor<'a, W, N>
 where
-    W: Waiter + 'a,
-    N: Notifier + Clone + Send + 'a,
+    W: Wait + 'a,
+    N: Notify + Clone + Send + 'a,
+{
+    pub fn new(size: usize, wait: W, notify: N) -> Self {
+        Self(Executor::new(size, wait, notify), ptr::null())
+    }
+
+    pub fn spawn<F, T>(&mut self, fut: F) -> Task<T>
+    where
+        F: Future<Output = T> + 'a,
+        T: 'a,
+    {
+        unsafe { self.0.spawn(fut) }
+    }
+
+    pub fn run<F, T>(&mut self, fut: F) -> T
+    where
+        F: Future<Output = T> + 'a,
+        T: 'a,
+    {
+        self.0.notify.prerun();
+
+        let result = unsafe {
+            self.0.run::<_, _, _, MutexSignal<_, _>>(
+                fut,
+                Rc::new(MutexSignal::<SingleThreadedMutex<_>, _>::new()),
+            )
+        };
+
+        self.0.notify.postrun();
+
+        result
+    }
+}
+
+pub struct SendableExecutor<'a, W, N, M>(Executor<'a, W, N>, PhantomData<fn() -> M>);
+
+impl<'a, W, N, M> SendableExecutor<'a, W, N, M>
+where
+    W: Wait + 'a,
+    N: Notify + Clone + Send + 'a,
+    M: MutexFamily + Send + Sync + 'a,
+{
+    pub fn new(size: usize, wait: W, notify: N) -> Self {
+        Self(Executor::new(size, wait, notify), PhantomData)
+    }
+
+    pub fn spawn<F, T>(&mut self, fut: F) -> Task<T>
+    where
+        F: Future<Output = T> + Send + 'a,
+        T: Send + 'a,
+    {
+        unsafe { self.0.spawn(fut) }
+    }
+
+    pub fn run<F, T>(&mut self, fut: F) -> T
+    where
+        F: Future<Output = T> + Send + 'a,
+        T: Send + 'a,
+    {
+        self.0.notify.prerun();
+
+        let result = unsafe {
+            self.0.run::<_, _, _, MutexSignal<_, _>>(
+                fut,
+                Arc::new(MutexSignal::<M::Mutex<State<T>>, _>::new()),
+            )
+        };
+
+        self.0.notify.postrun();
+
+        result
+    }
+}
+
+pub struct Executor<'a, W, N> {
+    queue: Arc<ArrayQueue<Runnable>>,
+    wait: W,
+    notify: N,
+    _lft: PhantomData<&'a ()>,
+}
+
+impl<'a, W, N> Executor<'a, W, N>
+where
+    W: Wait + 'a,
+    N: Notify + Clone + Send + 'a,
 {
     pub fn new(size: usize, waiter: W, notifier: N) -> Self {
         Self {
             queue: Arc::new(ArrayQueue::new(size)),
-            waiter,
-            notifier,
+            wait: waiter,
+            notify: notifier,
             _lft: PhantomData,
         }
     }
 
-    pub fn spawn<T>(&mut self, fut: impl Future<Output = T> + 'a) -> Task<T>
+    pub unsafe fn spawn<F, T>(&mut self, fut: F) -> Task<T>
     where
+        F: Future<Output = T> + 'a,
         T: 'a,
     {
         let schedule = {
             let queue = self.queue.clone();
-            let notifier = self.notifier.clone();
+            let notifier = self.notify.clone();
 
             move |runnable| {
                 queue.push(runnable).unwrap();
@@ -75,34 +161,35 @@ where
             }
         };
 
-        let (runnable, task) = unsafe { async_task::spawn_unchecked(fut, schedule) };
+        let (runnable, task) = async_task::spawn_unchecked(fut, schedule);
 
         runnable.schedule();
 
         task
     }
 
-    pub fn run<T>(&mut self, fut: impl Future<Output = T> + 'a) -> T
+    pub unsafe fn run<F, T, B, S>(&mut self, fut: F, signal_ref: B) -> T
     where
+        F: Future<Output = T> + 'a,
         T: 'a,
+        B: Borrow<S> + Clone + 'a,
+        S: Signal<Data = T>,
     {
-        let signal = Rc::new(MutexSignal::<SingleThreadedMutex<_>, _>::new());
-
         let _task = {
-            let signal = signal.clone();
+            let signal_ref = signal_ref.clone();
 
-            self.spawn(async move { signal.signal(fut.await) })
+            self.spawn(async move { signal_ref.borrow().signal(fut.await) })
         };
 
         loop {
-            if let Some(res) = signal.try_get() {
+            if let Some(res) = signal_ref.borrow().try_get() {
                 return res;
             }
 
             if let Some(runnable) = self.queue.pop() {
                 runnable.run();
             } else {
-                self.waiter.wait();
+                self.wait.wait();
             }
         }
     }
