f64d9aa81a81eb5731b5e6f2e28e6a5a2e65d307
ivmarkov <ivan.markov@gmail.com>
2022-04-30 23:19:27 +0300

    Fix no-std compilation

--- src/unblocker.rs
+++ src/unblocker.rs
@@ -22,65 +22,75 @@ pub mod asyncs {
             T: Send + 'static;
     }
 
-    #[derive(Clone)]
-    struct BlockingUnblocker;
+    #[cfg(feature = "alloc")]
+    use utils::*;
 
-    pub struct BlockingFuture<T> {
-        // TODO: Need to box or else we get rustc error:
-        // "type parameter `F` is part of concrete type but not used in parameter list for the `impl Trait` type alias"
-        computation: Option<Box<dyn FnOnce() -> T + Send + 'static>>,
-        _result: PhantomData<fn() -> T>,
-    }
+    #[cfg(feature = "alloc")]
+    mod utils {
+        extern crate alloc;
 
-    impl<T> BlockingFuture<T> {
-        pub fn new<F>(computation: F) -> Self
-        where
-            F: FnOnce() -> T + Send + 'static,
-            T: Send + 'static,
-        {
-            Self {
-                computation: Some(Box::new(computation)),
-                _result: PhantomData,
+        use alloc::boxed::Box;
+
+        #[derive(Clone)]
+        struct BlockingUnblocker;
+
+        pub struct BlockingFuture<T> {
+            // TODO: Need to box or else we get rustc error:
+            // "type parameter `F` is part of concrete type but not used in parameter list for the `impl Trait` type alias"
+            computation: Option<Box<dyn FnOnce() -> T + Send + 'static>>,
+            _result: PhantomData<fn() -> T>,
+        }
+
+        impl<T> BlockingFuture<T> {
+            pub fn new<F>(computation: F) -> Self
+            where
+                F: FnOnce() -> T + Send + 'static,
+                T: Send + 'static,
+            {
+                Self {
+                    computation: Some(Box::new(computation)),
+                    _result: PhantomData,
+                }
             }
         }
-    }
 
-    impl<T> Future for BlockingFuture<T>
-    where
-        T: Send,
-    {
-        type Output = T;
-
-        fn poll(
-            mut self: core::pin::Pin<&mut Self>,
-            _cx: &mut core::task::Context<'_>,
-        ) -> Poll<Self::Output> {
-            let computation = mem::replace(&mut self.computation, None);
-
-            if let Some(computation) = computation {
-                Poll::Ready((computation)())
-            } else {
-                unreachable!()
-            }
-        }
-    }
-
-    impl Unblocker for BlockingUnblocker {
-        type UnblockFuture<T>
+        impl<T> Future for BlockingFuture<T>
         where
             T: Send,
-        = BlockingFuture<T>;
-
-        fn unblock<F, T>(&self, f: F) -> Self::UnblockFuture<T>
-        where
-            F: FnOnce() -> T + Send + 'static,
-            T: Send + 'static,
         {
-            BlockingFuture::new(f)
-        }
-    }
+            type Output = T;
 
-    pub fn blocking_unblocker() -> impl Unblocker + Clone {
-        BlockingUnblocker
+            fn poll(
+                mut self: core::pin::Pin<&mut Self>,
+                _cx: &mut core::task::Context<'_>,
+            ) -> Poll<Self::Output> {
+                let computation = mem::replace(&mut self.computation, None);
+
+                if let Some(computation) = computation {
+                    Poll::Ready((computation)())
+                } else {
+                    unreachable!()
+                }
+            }
+        }
+
+        impl Unblocker for BlockingUnblocker {
+            type UnblockFuture<T>
+            where
+                T: Send,
+            = BlockingFuture<T>;
+
+            fn unblock<F, T>(&self, f: F) -> Self::UnblockFuture<T>
+            where
+                F: FnOnce() -> T + Send + 'static,
+                T: Send + 'static,
+            {
+                BlockingFuture::new(f)
+            }
+        }
+
+        pub fn blocking_unblocker() -> impl Unblocker + Clone {
+            BlockingUnblocker
+        }
     }
 }
