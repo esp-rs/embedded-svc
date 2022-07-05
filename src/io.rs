a0a2e8c047d61383a683a0168d7a11bca90c7c8e
Ivan Markov <ivan.markov@gmail.com>
2022-07-05 09:21:25 +0000

    Blocking wrappers on top of async APIs

--- src/io.rs
+++ src/io.rs
@@ -6,4 +6,37 @@ pub use embedded_io::*;
 pub mod asynch {
     pub use embedded_io::asynch::*;
     pub use embedded_io::*;
+
+    use crate::unblocker::asynch::{Blocker, Blocking};
+
+    impl<B, I> Io for Blocking<B, I>
+    where
+        I: Io,
+    {
+        type Error = I::Error;
+    }
+
+    impl<B, R> super::Read for Blocking<B, R>
+    where
+        B: Blocker,
+        R: Read,
+    {
+        fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error> {
+            self.0.block_on(self.1.read(buf))
+        }
+    }
+
+    impl<B, W> super::Write for Blocking<B, W>
+    where
+        B: Blocker,
+        W: Write,
+    {
+        fn write(&mut self, buf: &[u8]) -> Result<usize, Self::Error> {
+            self.0.block_on(self.1.write(buf))
+        }
+
+        fn flush(&mut self) -> Result<(), Self::Error> {
+            self.0.block_on(self.1.flush())
+        }
+    }
 }
