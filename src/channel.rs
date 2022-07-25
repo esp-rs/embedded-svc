fb4833a12dc54000995a291d0812d6cd883e55c6
Ivan Markov <ivan.markov@gmail.com>
2022-07-25 08:02:31 +0000

    &mut impl for Sender & Receiver

--- src/channel.rs
+++ src/channel.rs
@@ -12,6 +12,22 @@ pub mod asynch {
         fn send(&mut self, value: Self::Data) -> Self::SendFuture<'_>;
     }
 
+    impl<S> Sender for &mut S
+    where
+        S: Sender,
+    {
+        type Data = S::Data;
+
+        type SendFuture<'a>
+        where
+            Self: 'a,
+        = S::SendFuture<'a>;
+
+        fn send(&mut self, value: Self::Data) -> Self::SendFuture<'_> {
+            (*self).send(value)
+        }
+    }
+
     pub trait Receiver {
         type Data: Send;
 
@@ -20,5 +36,21 @@ pub mod asynch {
             Self: 'a;
 
         fn recv(&mut self) -> Self::RecvFuture<'_>;
+    }
+
+    impl<R> Receiver for &mut R
+    where
+        R: Receiver,
+    {
+        type Data = R::Data;
+
+        type RecvFuture<'a>
+        where
+            Self: 'a,
+        = R::RecvFuture<'a>;
+
+        fn recv(&mut self) -> Self::RecvFuture<'_> {
+            (*self).recv()
+        }
     }
 }
