abdb88f2280766a4bd9ae71fdab004ef55786fc2
Ivan Markov <ivan.markov@gmail.com>
2022-08-16 11:07:15 +0000

    Move json_io elsewhere

--- src/utils.rs
+++ src/utils.rs
@@ -3,10 +3,5 @@ pub mod asyncify;
 #[cfg(feature = "experimental")]
 pub mod http;
 pub mod io;
-#[cfg(all(
-    feature = "experimental",
-    any(feature = "json_io", feature = "json_io_core")
-))]
-pub mod json_io;
 pub mod mqtt;
 pub mod mutex;
