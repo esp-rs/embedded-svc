d52dc5ea28b6881d9337b366d9c4a65c9ea73984
Ivan Markov <ivan.markov@gmail.com>
2022-08-16 10:59:49 +0000

    Move generic utils elsewhere (rest, ghota); deprecated httpd

--- src/utils.rs
+++ src/utils.rs
@@ -1,11 +1,5 @@
 #[cfg(feature = "experimental")]
 pub mod asyncify;
-#[cfg(all(
-    feature = "experimental",
-    any(feature = "json_io", feature = "json_io_core"),
-    feature = "use_serde"
-))]
-pub mod ghota;
 #[cfg(feature = "experimental")]
 pub mod http;
 pub mod io;
@@ -16,10 +10,3 @@ pub mod io;
 pub mod json_io;
 pub mod mqtt;
 pub mod mutex;
-#[cfg(all(
-    feature = "experimental",
-    any(feature = "json_io", feature = "json_io_core"),
-    feature = "use_serde"
-))]
-pub mod rest;
-pub mod role;
