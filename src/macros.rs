51bb171faa24ad1d3d03df4a16339b707ead1664
ImUrX <urielfontan2002@gmail.com>
2022-11-25 13:31:17 -0300

    rename net_log to svc_log

--- src/macros.rs
+++ src/macros.rs
@@ -2,7 +2,7 @@
 #[cfg(not(test))]
 #[cfg(feature = "log")]
 #[macro_export]
-macro_rules! net_log {
+macro_rules! svc_log {
     (info, $($arg:expr),*) => { log::info!($($arg),*) };
     (warn, $($arg:expr),*) => { log::warn!($($arg),*) };
 }
@@ -10,22 +10,22 @@ macro_rules! net_log {
 #[cfg(test)]
 #[cfg(feature = "log")]
 #[macro_export]
-macro_rules! net_log {
+macro_rules! svc_log {
     (info, $($arg:expr),*) => { println!($($arg),*) };
     (warn, $($arg:expr),*) => { println!($($arg),*) };
 }
 
 #[cfg(feature = "defmt")]
 #[macro_export]
-macro_rules! net_log {
+macro_rules! svc_log {
     (info, $($arg:expr),*) => { defmt::info!($($arg),*) };
     (warn, $($arg:expr),*) => { defmt::warn!($($arg),*) };
 }
 
 #[cfg(not(any(feature = "log", feature = "defmt")))]
 #[macro_export]
-macro_rules! net_log {
+macro_rules! svc_log {
     ($level:ident, $($arg:expr),*) => {{ $( let _ = $arg; )* }}
 }
 
-pub(crate) use net_log;
+pub(crate) use svc_log;
