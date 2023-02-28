3c5a43236ce5227721ee968f91c4c39ed624c64d
Scott Mabin <scott@mabez.dev>
2023-02-28 20:09:29 +0000

    Use GAT + TAIT instead of AFIT

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,6 @@
     feature(generic_associated_types)
 )]
 #![cfg_attr(feature = "nightly", feature(type_alias_impl_trait))]
-#![cfg_attr(feature = "nightly", allow(incomplete_features))]
-#![cfg_attr(
-    feature = "nightly",
-    feature(async_fn_in_trait, impl_trait_projections)
-)]
 
 #[cfg(feature = "alloc")]
 #[allow(unused_imports)]
