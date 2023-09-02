32a4d3a609801369c609496359d418c15ec108c1
ivmarkov <ivan.markov@gmail.com>
2023-09-02 10:37:22 +0000

    (WIP) Switch to afit everywhere

--- src/executor.rs
+++ src/executor.rs
@@ -3,89 +3,17 @@ pub mod asynch {
     pub use unblocker::*;
 
     use core::fmt::Debug;
-    use core::future::Future;
-
-    pub trait Blocker {
-        fn block_on<F>(&self, future: F) -> F::Output
-        where
-            F: Future;
-    }
-
-    impl<B> Blocker for &B
-    where
-        B: Blocker,
-    {
-        fn block_on<F>(&self, future: F) -> F::Output
-        where
-            F: Future,
-        {
-            (*self).block_on(future)
-        }
-    }
 
     #[derive(Clone, Debug)]
     #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-    pub struct Blocking<B, T> {
-        pub blocker: B,
+    pub struct Unblocking<U, T> {
+        pub unblocker: U,
         pub api: T,
     }
 
-    impl<B, T> Blocking<B, T> {
-        pub const fn new(blocker: B, api: T) -> Self {
-            Self { blocker, api }
-        }
-    }
-
-    #[derive(Clone, Debug)]
-    pub struct RawBlocking<B, T> {
-        pub blocker: *const B,
-        pub api: *mut T,
-    }
-
-    impl<B, T> RawBlocking<B, T> {
-        pub const fn new() -> Self {
-            Self {
-                blocker: core::ptr::null(),
-                api: core::ptr::null_mut(),
-            }
-        }
-    }
-
-    impl<B, T> Default for RawBlocking<B, T> {
-        fn default() -> Self {
-            Self::new()
-        }
-    }
-
-    #[derive(Debug)]
-    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-    pub struct TrivialUnblocking<T> {
-        pub api: T,
-    }
-
-    impl<T> TrivialUnblocking<T> {
-        pub const fn new(api: T) -> Self {
-            Self { api }
-        }
-    }
-
-    #[derive(Debug)]
-    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-    pub struct RawTrivialUnblocking<T> {
-        pub api: *mut T,
-    }
-
-    impl<T> RawTrivialUnblocking<T> {
-        pub const fn new() -> Self {
-            Self {
-                api: core::ptr::null_mut(),
-            }
-        }
-    }
-
-    impl<T> Default for RawTrivialUnblocking<T> {
-        fn default() -> Self {
-            Self::new()
+    impl<U, T> Unblocking<U, T> {
+        pub const fn new(unblocker: U, api: T) -> Self {
+            Self { unblocker, api }
         }
     }
 
@@ -109,41 +37,6 @@ pub mod asynch {
             {
                 (*self).unblock(f).await
             }
-        }
-    }
-
-    #[derive(Clone, Debug)]
-    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-    pub struct Unblocking<U, T> {
-        pub unblocker: U,
-        pub api: T,
-    }
-
-    impl<U, T> Unblocking<U, T> {
-        pub const fn new(unblocker: U, api: T) -> Self {
-            Self { unblocker, api }
-        }
-    }
-
-    #[derive(Clone, Debug)]
-    #[cfg_attr(feature = "defmt", derive(defmt::Format))]
-    pub struct RawUnblocking<U, T> {
-        pub unblocker: *const U,
-        pub api: *mut T,
-    }
-
-    impl<U, T> RawUnblocking<U, T> {
-        pub fn new() -> Self {
-            Self {
-                unblocker: core::ptr::null(),
-                api: core::ptr::null_mut(),
-            }
-        }
-    }
-
-    impl<B, T> Default for RawUnblocking<B, T> {
-        fn default() -> Self {
-            Self::new()
         }
     }
 }
