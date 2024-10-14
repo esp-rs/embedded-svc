0d6be55f61d2954628f50b3ee3ffdf78300aa552
Dane Slattery <dane@polarmonitoring.com>
2024-10-14 11:48:13 +0200

    Add Eq and Hash

--- src/ota.rs
+++ src/ota.rs
@@ -4,7 +4,7 @@ use serde::{Deserialize, Serialize};
 use crate::io::{ErrorType, Read, Write};
 use crate::utils::io::*;
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[cfg_attr(feature = "use_serde", derive(Serialize, Deserialize))]
 pub struct Slot {
@@ -13,7 +13,7 @@ pub struct Slot {
     pub firmware: Option<FirmwareInfo>,
 }
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "defmt", derive(defmt::Format))]
 #[cfg_attr(feature = "use_serde", derive(Serialize, Deserialize))]
 pub struct FirmwareInfo {
